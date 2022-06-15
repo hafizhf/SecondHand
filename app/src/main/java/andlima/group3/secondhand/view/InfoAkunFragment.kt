da4bd27c9477dc1f6e20b65015012b2d36a3519f
Zulfa Iflakhul Fadilla <89766936+zulfaiflakhul@users.noreply.github.com>
2022-06-15 21:36:09 +0700

    Update Info Akun
    Interface Update Info Akun

---
+++ app/src/main/java/andlima/group3/secondhand/view/InfoAkunFragment.kt
@@ -0,0 +1,20 @@
+package andlima.group3.secondhand.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import andlima.group3.secondhand.R
+
+class InfoAkunFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_info_akun, container, false)
+    }
+
+}
