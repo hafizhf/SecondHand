73b5eb64b182a0a5c24122c56f08edb8f9b465a5
hafizhf <hafizhfy@upi.edu>
2022-06-16 18:22:58 +0700

    Detail product page

--- app/src/main/java/andlima/group3/secondhand/view/DetailFragment.kt
+++ app/src/main/java/andlima/group3/secondhand/view/DetailFragment.kt
@@ -7,12 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import andlima.group3.secondhand.R
 
-class InfoPenawarFragment : Fragment() {
+class DetailFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info_penawar, container, false)
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
