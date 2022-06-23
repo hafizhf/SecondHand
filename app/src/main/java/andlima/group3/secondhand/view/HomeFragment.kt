fa12c79c02a8432c6d1a78587e22e128fe1d7758
hafizhf <hafizhfy@upi.edu>
2022-06-24 00:37:30 +0700

    Implement search item

--- app/src/main/java/andlima/group3/secondhand/view/HomeFragment.kt
+++ app/src/main/java/andlima/group3/secondhand/view/HomeFragment.kt
@@ -9,9 +9,22 @@ import android.view.ViewGroup
 import andlima.group3.secondhand.R
 import andlima.group3.secondhand.func.getDeviceScreenHeight
 import andlima.group3.secondhand.func.isScrollReachedBottom
+import andlima.group3.secondhand.func.toast
 import andlima.group3.secondhand.view.adapter.AdapterHomePager
+import andlima.group3.secondhand.view.adapter.SearchResultAdapter
+import andlima.group3.secondhand.viewmodel.BuyerViewModel
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import androidx.cardview.widget.CardView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_home.*
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,5 +49,98 @@ class HomeFragment : Fragment() {
         isScrollReachedBottom(scroll_view_home) {
             MarketApplication.homeFragmentReachedBottom.value = it
         }
+
+        homeSearchView()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun homeSearchView() {
+        val makeSearchEvent : CardView = requireView().findViewById(R.id.container_search_event)
+        val searchResultContainer : LinearLayout = requireView().findViewById(R.id.container_search_result)
+        val a : SearchView = requireView().findViewById(R.id.home_search_bar)
+
+        val searchPlaceholder : TextView = requireView().findViewById(R.id.tv_search_placeholder)
+
+        val paramsResultContainer: ViewGroup.LayoutParams = searchResultContainer.layoutParams
+        paramsResultContainer.height = getDeviceScreenHeight(requireActivity())
+        searchResultContainer.layoutParams = paramsResultContainer
+
+        // Observe focus change on search view
+        a.setOnQueryTextFocusChangeListener { _, focused ->
+
+            // Disable scroll for home when focus on search
+            scroll_view_home.setOnTouchListener { _, _ -> focused }
+
+            if (focused) {
+                a.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextSubmit(p0: String?): Boolean {
+                        toast(requireContext(), "Belum bisa submit ges")
+                        return false
+                    }
+
+                    @SuppressLint("SetTextI18n")
+                    override fun onQueryTextChange(p0: String?): Boolean {
+                        getSearchResult(p0!!)
+                        if (p0 != "") {
+                            searchPlaceholder.text = p0
+                        } else {
+                            searchPlaceholder.text = "Cari di Second Chance"
+                        }
+                        return false
+                    }
+                })
+            }
+        }
+
+        // Dim screen when search bar clicked
+        makeSearchEvent.setOnClickListener {
+            searchResultContainer.visibility = View.VISIBLE
+            a.requestFocus()
+
+            showSoftKeyboard()
+        }
+
+        // Hide search container when user click on dim side
+        searchResultContainer.setOnClickListener {
+            searchResultContainer.visibility = View.GONE
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun getSearchResult(userInput: String) {
+        val recyclerView: RecyclerView = requireView().findViewById(R.id.rv_search_result)
+        val searchAdapter = SearchResultAdapter() {
+            toast(requireContext(), "You thought this was $it? But it was me, Dio!")
+        }
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = searchAdapter
+
+        val viewModel = ViewModelProvider(this)[BuyerViewModel::class.java]
+        viewModel.allProductData.observe(this, {
+            if (it != null) {
+                val listProduct = it
+                val listProductName : MutableList<String> = mutableListOf()
+                listProduct.forEach { item ->
+                    if (item.name != null) {
+                        listProductName.add(item.name)
+                    }
+                }
+                val listProductNameFiltered = listProductName.filter { item ->
+                    item.contains(userInput, ignoreCase = true)
+                }
+                searchAdapter.setResultList(listProductNameFiltered)
+                searchAdapter.notifyDataSetChanged()
+            } else {
+                // Something to show when there is no product
+            }
+        })
+    }
+
+    private fun showSoftKeyboard() {
+        val view = activity?.currentFocus
+        val methodManager = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        assert(view != null)
+        methodManager.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
     }
 }
---
+++ app/src/main/java/andlima/group3/secondhand/view/adapter/SearchResultAdapter.kt
@@ -0,0 +1,44 @@
+package andlima.group3.secondhand.view.adapter
+
+import andlima.group3.secondhand.R
+import andlima.group3.secondhand.model.home.BuyerProductItem
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_search_result_simple.view.*
+
+class SearchResultAdapter(private var onClick: (String) -> Unit) : RecyclerView.Adapter<SearchResultAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    private var resultList: List<String>? = null
+
+    fun setResultList(result: List<String>) {
+        this.resultList = result
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_search_result_simple, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemView.apply {
+            tv_search_result_found.text = resultList!![position]
+
+            item_search_result_found.setOnClickListener {
+                onClick(resultList!![position])
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return if (resultList != null)
+            resultList!!.size
+        else
+            0
+    }
+}
